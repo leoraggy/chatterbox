c6ee0d70296a06f0916bcfaa884050eb0de2e2a8
leoraggy <leoragual35@gmail.com>
2025-12-04 18:24:52 -0800

    Added error handling for if args.length isn't equal to 4

--- src/ChatterboxClient.java
+++ src/ChatterboxClient.java
@@ -127,7 +127,12 @@ public class ChatterboxClient {
     public static ChatterboxOptions parseArgs(String[] args) throws IllegalArgumentException {
         // TODO: read args in the required order and return new ChatterboxOptions(host, port, username, password)
         // Remove this exception
-        throw new UnsupportedOperationException("Argument parsing not yet implemented. Implement parseArgs and remove this exception");
+        // throw new UnsupportedOperationException("Argument parsing not yet implemented. Implement parseArgs and remove this exception");
+
+        if(args.length != 4) throw new IllegalArgumentException("Argument must be 4 inputs");
+
+        
+       
     }
 
     /**
